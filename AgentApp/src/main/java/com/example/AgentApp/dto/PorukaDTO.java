146adda260a71cbfb9a100d6b02adcdbcac05ecd
bojan <bojan@Bojan>
2018-06-23 17:48:48 +0200

    agent

--- AgentApp/src/main/java/com/example/AgentApp/dto/PorukaDTO.java
+++ AgentApp/src/main/java/com/example/AgentApp/dto/PorukaDTO.java
@@ -3,14 +3,75 @@ package com.example.AgentApp.dto;
 
 public class PorukaDTO {
 
+	public PorukaDTO(Long id, String sadrzaj, boolean poslato, String datum, UserDTO posiljalac) {
+		super();
+		this.id = id;
+		this.sadrzaj = sadrzaj;
+		this.poslato = poslato;
+		this.datum = datum;
+		this.posiljalac = posiljalac;
+	}
 	private Long id;
 	private String sadrzaj;
-	private boolean poslata;
+	private boolean poslato;
 	private String datum;
 	private UserDTO posiljalac;
 	
 	
-	
+	public PorukaDTO() {
+		
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getSadrzaj() {
+		return sadrzaj;
+	}
+
+
+	public void setSadrzaj(String sadrzaj) {
+		this.sadrzaj = sadrzaj;
+	}
+
+
+	public boolean isPoslato() {
+		return poslato;
+	}
+
+
+	public void setPoslato(boolean poslato) {
+		this.poslato = poslato;
+	}
+
+
+	public String getDatum() {
+		return datum;
+	}
+
+
+	public void setDatum(String datum) {
+		this.datum = datum;
+	}
+
+
+	public UserDTO getPosiljalac() {
+		return posiljalac;
+	}
+
+
+	public void setPosiljalac(UserDTO posiljalac) {
+		this.posiljalac = posiljalac;
+	}
+
 	
 	
 	
